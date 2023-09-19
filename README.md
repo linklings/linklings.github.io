1c9a0e3586c91eb55fa669e4cd2c4457643ac993
Kit Alderson <linklings@github.com>
2023-09-18 20:14:29 -0500

    try the preview

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
-# The Linklings
-
-This is the home of the Collected Papers. Herein the reader will find a healthy variety of new art, though we must acknowledge our great indebtedness to those who have gone before. I hope you find yourself just as inspired as we have been!
-
-## TODO
-- upgrade website
-- add content
-- update README
+# kc-equine-services
+Knowledgeable consultation to help you make the most of your equine experience
--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,10 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/linklings.jpg') {
-        myImage.setAttribute('src', 'images/cslewis.jpg');
-    } else {
+    if(mySrc === 'images/swanee_headshot.jpg') {
         myImage.setAttribute('src', 'images/linklings.jpg');
+    } else {
+        myImage.setAttribute('src', 'images/swanee_headshot.jpg');
     }
 }
 
@@ -20,7 +20,7 @@ myImage.onclick = function() {
 //         localStorage.setItem('name', myName);
 //         myHeading.textContent = 'Welcome to the Linklings, ' + myName;
 //     }
-    
+
 // }
 
 // if(!localStorage.getItem('name')) {
